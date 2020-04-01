13fb1f56a7912aac10cd04eff2b211be3e22a2e0
Daniel <danielminami@hotmail.com>
2020-04-01 17:05:07 -0400

    Custom Retrofit class with Logging Interceptor.

--- app/src/main/java/com/minamid/accessiblememorygame/base/CustomRetrofit.java
+++ app/src/main/java/com/minamid/accessiblememorygame/base/CustomRetrofit.java
@@ -1,16 +1,31 @@
 package com.minamid.accessiblememorygame.base;
 
+import com.minamid.accessiblememorygame.util.Config;
+import com.minamid.accessiblememorygame.util.LogInterceptor;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class CustomRetrofit {
 
-    private Retrofit retrofit;
+    public static Retrofit retrofit;
 
-    public Retrofit setup(){
+    public static Retrofit setup(){
+
+        LogInterceptor logInterceptor = new LogInterceptor();
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(logInterceptor)
+                .build();
 
         //TODO: Create the custom Retrofit Class with interceptor
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl(Config.getInstance().getUrl())
+                .addConverterFactory(GsonConverterFactory.create());
 
-        return retrofit;
+        return retrofit = builder.build();
     }
 
 }
--- app/src/main/java/com/minamid/accessiblememorygame/util/LogInterceptor.java
+++ app/src/main/java/com/minamid/accessiblememorygame/util/LogInterceptor.java
@@ -1,16 +1,23 @@
 package com.minamid.accessiblememorygame.util;
 
+import android.util.Log;
+
 import java.io.IOException;
 
 import okhttp3.Interceptor;
+import okhttp3.Request;
 import okhttp3.Response;
 
 public class LogInterceptor implements Interceptor {
     @Override
     public Response intercept(Chain chain) throws IOException {
 
-        //TODO: Recreate this logger Interceptor
+        Request request = chain.request();
+        Log.d("REQUEST", "--> " + request.body().toString());
 
-        return null;
+        Response response = chain.proceed(request);
+        Log.d("RESPONSE", "<-- " + response.code() + " " + response.body().string());
+
+        return response;
     }
 }
